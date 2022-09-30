42d9d964425f1fe34285a259de653a5078f68a3b
Alkoun-web-design <113122406+Alkoun-web-design@users.noreply.github.com>
2022-09-30 18:10:22 +0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
+
+// Space Animation
 const spacerotation = anime({
     targets: '.space',
     translateX: 1440,
     duration: 40000,
     easing: 'linear',
     loop: true,
+});
+
+
+// Navigation button hover
+const element = document.querySelector(".nav-buton");
+
+element.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+});
+
+element.addEventListener("mouseout", event => {
+  console.log("Mouse out");
 });
