afd0710d158889862a19311ed7ea50032092b6fe
Alkoun-web-design <113122406+Alkoun-web-design@users.noreply.github.com>
2022-10-04 21:32:22 +0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
 // Space Animation
-const spacerotation = anime({
+const spaceRotation = anime({
     targets: '.space',
     translateX: 1440,
     duration: 40000,
@@ -9,8 +9,8 @@ const spacerotation = anime({
 });
 
 
-// Navigation button hover
-const element = document.querySelector(".nav-buton");
+/*// Navigation button hover
+const hoverAnime = document.querySelector(".nav-buton");
 
 element.addEventListener("mouseover", event => {
   console.log("Mouse in");
@@ -19,3 +19,49 @@ element.addEventListener("mouseover", event => {
 element.addEventListener("mouseout", event => {
   console.log("Mouse out");
 });
+
+
+//Content Appearing and Disappearing
+var animation = anime({
+  targets: '.play-pause-demo .el',
+  translateX: 270,
+  delay: function(el, i) { return i * 100; },
+  direction: 'alternate',
+  autoplay: false,
+  loop: false,
+  easing: 'easeIn'
+});
+
+document.querySelector('.play-pause-demo .play').onclick = animation.play;
+document.querySelector('.play-pause-demo .pause').onclick = animation.pause;
+
+
+//Another approach
+logoTimeline
+.add({
+    targets: text1,
+    duration: 700,
+    delay: function(el, index) { return index*50; },
+    opacity: 1,
+
+    easing: 'easeOutCirc',
+    translateX: function(el, index) {
+        return [(-50+index*10),0]
+    },
+    offset:0,
+    complete: function(anim) {
+        logoTimeline.remove();
+    }
+});
+
+
+//Sjowing elements
+const element = document.querySelector("#box");
+
+element.style.display = "block";
+
+
+//Hiding Elements
+const element = document.querySelector("#box");
+
+element.style.display = "none";*/
