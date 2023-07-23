6dc044d0c8b26193d4aa1398c43d0561be9ecdc4
Your Name~ <alkoun.web.designs@proton.me>
2023-07-23 14:01:04 +0500

    added id checker

--- assets/js/script.js
+++ assets/js/script.js
@@ -244,4 +244,6 @@ document.getElementById("projects-btn").addEventListener('click', showPage);
 document.getElementById("contact-us-btn").addEventListener('click', showPage);
 document.getElementById("privacy-policy-btn").addEventListener('click', showPage);
 
-
+document.querySelector('nody').addEventListener('click', ()=>{
+  console.log(this.id);
+});
